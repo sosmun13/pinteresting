0541bbef06c29b54da33ebe12b8f6706d28a2f2a
Stephen Osmun <Sosmun13@gmail.com>
2014-06-22 17:04:40 -0700

    Added Devise Gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  #update with local host website in place of 'localhost:3000'
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  
 end
